782e07a326836ec1a26c6324771e03dcc3d32706
Oskar Allerslev <oskar.m1660@gmail.com>
2026-04-30 12:06:26 +0200

    fix: småfejl

--- week-3-4/headerfiles/headerfiles/Student.cpp
+++ week-3-4/headerfiles/headerfiles/Student.cpp
@@ -22,11 +22,7 @@ Student::Student(std::string name, double fines, double fees)
 
 void Student::SetLibraryFines(double amount)
 {
-    if(amount <= 0) 
-    {
-        library_fines = 0;
-    }
-    else 
+    if(amount > 0.0)
     {
         library_fines = amount;
     }
