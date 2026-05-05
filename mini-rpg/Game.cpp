5aa3fad59561fe1276cf30163f333633e0c7e61f
Oskar Allerslev <oskar.m1660@gmail.com>
2026-05-05 20:26:28 +0200

    feat: tilføjet boks til heal eller angribe

--- mini-rpg/Game.cpp
+++ mini-rpg/Game.cpp
@@ -1,127 +1,188 @@
 #include "Game.hpp"
+#include "SFML/Graphics/Color.hpp"
+#include "SFML/System/Vector2.hpp"
+#include "SFML/Window/Keyboard.hpp"
 #include <iostream>
 
 // constructor
-Game::Game() 
-    : window(sf::VideoMode(1000, 800), "MINI-RPG"),
-      player("Oskar", 100, 20),
-      enemy("Dracula", 80, 15),
-      currentState(PLAYER_TURN),
-      enemyDelay(0.8f),
-      timer(0.f),
-      playerColor(sf::Color::White),
-      enemyColor(sf::Color::White)
-{
-    if (!font.loadFromFile("Assets/Fonts/font.TTF")) {
-        std::cout << "Kunne ikke loade font!\n";
-    }
-    if (!playerTex.loadFromFile("Assets/Sprites/player.png") || 
-        !enemyTex.loadFromFile("Assets/Sprites/vampire.png")) {
-        std::cout << "Kunne ikke loade sprites!\n";
-    }
+Game::Game()
+    : window(sf::VideoMode(1000, 800), "MINI-RPG"), player("Oskar", 100, 20),
+      enemy("Dracula", 80, 10), currentState(PLAYER_TURN), enemyDelay(0.8f),
+      timer(0.f), playerColor(sf::Color::White), enemyColor(sf::Color::White),
+      currentMenuIndex(0) {
+  if (!font.loadFromFile("Assets/Fonts/font.TTF")) {
+    std::cout << "Kunne ikke loade font!\n";
+  }
+  if (!playerTex.loadFromFile("Assets/Sprites/player.png") ||
+      !enemyTex.loadFromFile("Assets/Sprites/vampire.png")) {
+    std::cout << "Kunne ikke loade sprites!\n";
+  }
 
-    playerSprite.setTexture(playerTex);
-    playerSprite.setPosition(150.f, 400.f);
-    playerSprite.setScale(1.0f, 1.0f);
+  playerSprite.setTexture(playerTex);
+  playerSprite.setPosition(150.f, 400.f);
+  playerSprite.setScale(1.0f, 1.0f);
 
-    enemySprite.setTexture(enemyTex);
-    enemySprite.setPosition(650.f, 400.f);
-    enemySprite.setScale(1.0f, 1.0f);
+  enemySprite.setTexture(enemyTex);
+  enemySprite.setPosition(650.f, 400.f);
+  enemySprite.setScale(1.0f, 1.0f);
 
-    infoText.setFont(font);
-    infoText.setCharacterSize(24);
-    infoText.setFillColor(sf::Color::White);
-    infoText.setPosition(20.f, 20.f);
+  infoText.setFont(font);
+  infoText.setCharacterSize(24);
+  infoText.setFillColor(sf::Color::White);
+  infoText.setPosition(20.f, 20.f);
 
-    actionLog = "A wild monster attacks!\nPress space to attack! (H to Heal)";
+  actionLog = "A wild monster attacks!\nPress space to attack! (H to Heal)";
+
+  setupMenu();
 }
 
 // game loop
 void Game::run() {
-    while (window.isOpen()) {
-        float dt = gameClock.restart().asSeconds();
-        processEvents();
-        update(dt);
-        render();
-    }
+  while (window.isOpen()) {
+    float dt = gameClock.restart().asSeconds();
+    processEvents();
+    update(dt);
+    render();
+  }
 }
 
-// input handler 
+// input handler
 void Game::processEvents() {
-    sf::Event event;
-    while (window.pollEvent(event)) {
-        if (event.type == sf::Event::Closed) {
-            window.close();
-        }
-
-        if (currentState == PLAYER_TURN && event.type == sf::Event::KeyPressed) {
-            if (event.key.code == sf::Keyboard::Space) {
-                player.Attack(enemy);
-                enemyColor = sf::Color::Red;
-                timer = 0.f;
-                actionLog = "You attacked " + enemy.GetName() + "!";
-                
-                if (!enemy.IsAlive()) {
-                    actionLog += "\nYou defeated the monster!";
-                    currentState = GAME_OVER;
-                } else {
-                    currentState = ENEMY_TURN;
-                }
-            }
-            else if (event.key.code == sf::Keyboard::H) {
-                player.Heal(25);
-                actionLog = "You healed yourself!";
-                currentState = ENEMY_TURN;
-                timer = 0.f;
-            }
-        }
+  sf::Event event;
+  while (window.pollEvent(event)) {
+    if (event.type == sf::Event::Closed) {
+      window.close();
     }
+
+    if (currentState == PLAYER_TURN && event.type == sf::Event::KeyPressed) {
+      if (event.key.code == sf::Keyboard::Up) {
+        currentMenuIndex--;
+        if (currentMenuIndex < 0) {
+          currentMenuIndex = menuTexts.size() - 1;
+        }
+      } else if (event.key.code == sf::Keyboard::Down) {
+        currentMenuIndex++;
+        if (currentMenuIndex >= menuTexts.size()) {
+          currentMenuIndex = 0;
+        }
+      } else if (event.key.code == sf::Keyboard::Space ||
+                 event.key.code == sf::Keyboard::Enter) {
+        if (currentMenuIndex == 0) {
+          player.Attack(enemy);
+          enemyColor = sf::Color::Red;
+          timer = 0.f;
+          actionLog = "You attacked " + enemy.GetName() + "!";
+
+          if (!enemy.IsAlive()) {
+            actionLog += "\nYou defeated the monster!";
+            currentState = GAME_OVER;
+          } else {
+            currentState = ENEMY_TURN;
+          }
+        } else if (currentMenuIndex == 1) {
+
+          player.Heal(25);
+          actionLog = "You healed yourself!";
+          currentState = ENEMY_TURN;
+          timer = 0.f;
+        }
+      }
+    }
+  }
 }
 
-//  logix and enemy turn
+//  logic and enemy turn
 void Game::update(float dt) {
-    if (currentState == ENEMY_TURN) {
-        timer += dt;
-        if (timer >= enemyDelay) {
-            enemy.Attack(player);
-            playerColor = sf::Color::Red;
-            actionLog = enemy.GetName() + " attacked you!";
-            
-            if (!player.IsAlive()) {
-                actionLog += "\nYOU DIED";
-                currentState = GAME_OVER;
-            } else {
-                currentState = PLAYER_TURN;
-            }
-            timer = 0.f;
-        }
-    }
+  if (currentState == ENEMY_TURN) {
+    timer += dt;
+    if (timer >= enemyDelay) {
+      enemy.Attack(player);
+      playerColor = sf::Color::Red;
+      actionLog = enemy.GetName() + " attacked you!";
 
-    // fade back to white 
-    if (playerColor.g < 255) {
-        int val = std::min(255, (int)(playerColor.g + 10));
-        playerColor = sf::Color(255, val, val);
+      if (!player.IsAlive()) {
+        actionLog += "\nYOU DIED";
+        currentState = GAME_OVER;
+      } else {
+        currentState = PLAYER_TURN;
+      }
+      timer = 0.f;
     }
-    if (enemyColor.g < 255) {
-        int val = std::min(255, (int)(enemyColor.g + 10));
-        enemyColor = sf::Color(255, val, val);
+  }
+
+  // fade back to white
+  if (playerColor.g < 255) {
+    int val = std::min(255, (int)(playerColor.g + 10));
+    playerColor = sf::Color(255, val, val);
+  }
+  if (enemyColor.g < 255) {
+    int val = std::min(255, (int)(enemyColor.g + 10));
+    enemyColor = sf::Color(255, val, val);
+  }
+
+  // update the menu visually 
+  if (currentState == PLAYER_TURN)
+  {
+    for (size_t i = 0; i < menuTexts.size(); ++i)
+    {
+      if (i == currentMenuIndex) {
+        menuTexts[i].setFillColor(sf::Color::Yellow);
+        menuTexts[i].setString(std::string("> ") + (i == 0 ? "Attack" : "Heal"));
+      } else {
+        menuTexts[i].setFillColor(sf::Color::White);
+        menuTexts[i].setString(std::string("  ") + (i == 0 ? "Attack" : "Heal"));
+      }
     }
+  }
 
-    // update text and colors
-    std::string uiString = actionLog + "\n\n";
-    uiString += player.GetName() + " HP: " + std::to_string(player.GetHealth()) + "\n";
-    uiString += enemy.GetName() + " HP: " + std::to_string(enemy.GetHealth());
-    infoText.setString(uiString);
 
-    playerSprite.setColor(playerColor);
-    enemySprite.setColor(enemyColor);
+
+  // update text and colors
+  std::string uiString = actionLog + "\n\n";
+  uiString +=
+      player.GetName() + " HP: " + std::to_string(player.GetHealth()) + "\n";
+  uiString += enemy.GetName() + " HP: " + std::to_string(enemy.GetHealth());
+  infoText.setString(uiString);
+
+  playerSprite.setColor(playerColor);
+  enemySprite.setColor(enemyColor);
 }
 
 // render
 void Game::render() {
-    window.clear(sf::Color(30, 30, 30));
-    window.draw(playerSprite);
-    window.draw(enemySprite);
-    window.draw(infoText);
-    window.display();
+  window.clear(sf::Color(30, 30, 30));
+  window.draw(playerSprite);
+  window.draw(enemySprite);
+  window.draw(infoText);
+  if (currentState == PLAYER_TURN)
+  {
+    window.draw(menuBox);
+    for (auto& text : menuTexts)
+    {
+        window.draw(text);
+    }
+  }
+
+
+  window.display();
+}
+
+// setup menu function
+void Game::setupMenu() {
+  menuBox.setSize(sf::Vector2f(300.f, 150.f));
+  menuBox.setFillColor(
+      sf::Color(20, 20, 20, 230)); // Mørkegrå, lidt gennemsigtig
+  menuBox.setOutlineColor(sf::Color::White);
+  menuBox.setOutlineThickness(3.f);
+  menuBox.setPosition(50.f, 600.f);
+
+  std::vector<std::string> options = {"Attack", "Heal"};
+  for (size_t i = 0; i < options.size(); ++i) {
+    sf::Text text;
+    text.setFont(font);
+    text.setCharacterSize(28);
+    text.setFillColor(sf::Color::White);
+    text.setPosition(80.f, 630.f + i * 50.f);
+    menuTexts.push_back(text);
+  }
 }
