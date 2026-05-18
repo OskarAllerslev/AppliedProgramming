2904d889b5ddb14c7c18a94fa9846bfd3595a3c4
Oskar Allerslev <oskar.m1660@gmail.com>
2026-05-18 10:38:37 +0200

    feat: flyttet det hele til .cpp filen da det er den der skal afleveres

--- week-7-8/NearestNeighbours.cpp
+++ week-7-8/NearestNeighbours.cpp
@@ -1,24 +1,100 @@
-
-#include "NearestNeighbours.hpp"
 #include "armadillo.hpp"
+#include <iostream>
+#include <string>
 
+template <typename T> 
+class NearestNeigbours {
+private:
+    arma::mat X_train;
+    arma::mat X_test;
+    arma::mat Y_train; // 0 or 1
+
+public:
+    bool load_data(const std::string &x_train_file,
+                   const std::string &x_test_file,
+                   const std::string &y_train_file);
+
+    void print_dimensions() const;
+
+    void knn_metod();
+};
+
+template <typename T>
+bool NearestNeigbours<T>::load_data(const std::string &x_train_file,
+                                    const std::string &x_test_file,
+                                    const std::string &y_train_file) 
+{
+    if (!X_train.load(x_train_file) || !X_test.load(x_test_file) || !Y_train.load(y_train_file))
+    {
+        std::cerr << "Error loading one or more of the datasets" << std::endl;
+        return false;
+    }
+    return true;
+}
+
+template<typename T>
+void NearestNeigbours<T>::print_dimensions() const
+{
+    std::cout << "--- Dataset Dimensions ---" << std::endl;
+    std::cout << "X_train : " << X_train.n_rows << "x" << X_train.n_cols << std::endl;
+    std::cout << "Y_train : " << Y_train.n_elem << " elements" << std::endl;
+    std::cout << "X_test   : " << X_test.n_rows << "x" << X_test.n_cols << std::endl;
+}
+
+template<typename T>
+void NearestNeigbours<T>::knn_metod()
+{
+    unsigned int k = 5;
+    arma::vec Y_pred(X_test.n_rows);
+
+    for (size_t j = 0; j < X_test.n_rows; ++j)
+    {
+        arma::rowvec x_test_point = X_test.row(j);
+        arma::vec distances(X_train.n_rows);
+
+        for (size_t i = 0; i < X_train.n_rows; ++i)
+        {
+            distances(i) = arma::norm(X_train.row(i) - x_test_point);
+        }
+
+        arma::uvec sorted_indices = arma::sort_index(distances);
+
+        double label_sum = 0.0;
+        for (unsigned int n = 0; n < k; ++n)
+        {
+            size_t nearest_idx = sorted_indices(n);
+            label_sum += Y_train(nearest_idx);
+        }
+
+        if (label_sum > 0)
+        {
+            Y_pred(j) = 1.0;
+        } 
+        else 
+        {
+            Y_pred(j) = -1.0;
+        }
+    }
+
+    if (Y_pred.save("NN.dat", arma::raw_ascii))
+    {
+        std::cout << "Predictions written sucessfully" << std::endl;
+    } 
+    else 
+    {
+        std::cerr << "Error: could not write to NN.dat" << std::endl;
+    }
+}
 
 int main()
 {
-    
     NearestNeigbours<double> knn_model;
 
-    knn_model.load_data("dataX.dat", "dataXtest.dat", "dataY.dat");
+    if (knn_model.load_data("dataX.dat", "dataXtest.dat", "dataY.dat"))
+    {
+        knn_model.print_dimensions();
+        knn_model.knn_metod();
+    }
 
-    knn_model.print_dimensions();
-
-    
-
-
-
-
-
-
-    
     return 0;
 }
--- week-7-8/NearestNeighbours.hpp
+++ week-7-8/NearestNeighbours.hpp
@@ -2,7 +2,9 @@
 #define NearestNeighboursdef
 
 #include "armadillo.hpp"
+#include <cstddef>
 #include <string>
+#include <vector>
 
 template <typename T> class NearestNeigbours {
 private:
@@ -52,7 +54,7 @@ void NearestNeigbours<T>::print_dimensions() const
 }
 
 template<typename T>
-void knn_metod()
+void NearestNeigbours<T>::knn_metod()
 {
     /*
     we need to assign the appropriate label to each point in X_test.
@@ -61,14 +63,51 @@ void knn_metod()
     Write a new file "NN.dat" containing the assigned labels in the same
     format as "dataY.dat".
     */
-    
     // in both X_test and X_train we have a set of features 
     // we should take the distance between these points
+    unsigned int k = 5;
+    arma::vec Y_pred(X_test.n_rows);
 
-    double distance = 0.0;
+    for (size_t j = 0; j < X_test.n_rows; ++j)
+    {
+        arma::rowvec x_test_point = X_test.row(j);
 
+        arma::vec distances(X_train.n_rows);
 
+        for (size_t i = 0; i < X_train.n_rows; ++i)
+        {
+            distances(i) = arma::norm(X_train.row(i) - x_test_point);
+        }
 
+        arma::uvec sorted_indices = arma::sort_index(distances);
+
+        // make the majority vote over the k closets 
+        double label_sum = 0.0;
+        for (int n = 0; n < k; ++n)
+        {
+            size_t nearest_idx = sorted_indices(n);
+            label_sum += Y_train(nearest_idx);
+        }
+
+        if (label_sum > 0)
+        {
+            Y_pred(j) = 1;
+        } 
+        else 
+        {
+            Y_pred(j) = -1.0;
+        }
+
+        if (Y_pred.save("NN.dat", arma::raw_ascii))
+        {
+            std::cout << "Predictions written sucessfully" << std::endl;
+        } 
+        else 
+        {
+            std::cout << "Error: could not write to NN.dat" << std::endl;
+        }
+
+    }
 }
 
 
